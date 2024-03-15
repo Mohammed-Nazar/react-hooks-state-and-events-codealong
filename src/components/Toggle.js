3ecb96bd3148e2ae9e9e5ad5ec6a9a85666e656d
Mohammed Nazar <x3raqe@gmail.com>
2024-03-16 01:16:04 +0300

    Completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn , setIsOn] = useState(false)
+  const color = isOn ? "red" : "white";
+  return <button style={{
+    backgroundColor: color
+  }} onClick={()=> setIsOn((prev)=> !prev)}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
